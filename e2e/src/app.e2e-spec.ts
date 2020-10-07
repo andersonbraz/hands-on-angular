d30b42e8315332e6da61fd724373846f9f04fda4
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-10-06 23:03:36 -0300

    Up Files!

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-angular-app app is running!');
+    expect(page.getTitleText()).toEqual('hands-on-angular app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContatosListagemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
